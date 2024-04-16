41b1210118c4d1bc3c6766c488d6fca2f9a99165
Tayuya <lyna-93@mail.ru>
2024-04-16 23:18:06 +0500

    section 7 (lessons <62)

--- script.js
+++ script.js
@@ -263,25 +263,74 @@
 // 	}
 // }
 
-//Задача по удвоению массива без изменения текущего, а с возвратом нового
-let a = [1, 2, 3]
-let b = [5, "hello", 6]
+// //Задача по удвоению массива без изменения текущего, а с возвратом нового
+// let a = [1, 2, 3]
+// let b = [5, "hello", 6]
 
-Array.prototype.double = function() {
-	let newArray = this.map(function(item) {
-		if (typeof item === 'number') {
-			return Math.pow(item, 2)
-		}
-		if (typeof item === 'string') {
-			 return item += item
-		}
-	})
+// Array.prototype.double = function() {
+// 	let newArray = this.map(function(item) {
+// 		if (typeof item === 'number') {
+// 			return Math.pow(item, 2)
+// 		}
+// 		if (typeof item === 'string') {
+// 			 return item += item
+// 		}
+// 	})
 
-	return newArray
-}
+// 	return newArray
+// }
 
-let newA = a.double()
-let newB = b.double()
+// let newA = a.double()
+// let newB = b.double()
 
-console.log('A', newA.double())
-console.log('B', newB)
+// console.log('A', newA.double())
+// console.log('B', newB)
+
+// //spread and rest 
+// const form = document.querySelector('form')
+
+// form.addEventListener('submit', event => {
+
+//   event.preventDefault()
+
+//   const title = form.title.value
+//   const text = form.text.value
+//   const description = form.description.value
+
+//   saveForm(title, text, description)
+
+// })
+
+// // Spread
+// // function saveForm(data) {
+// //   const formData = {
+// //     date: new Date().toLocaleDateString(),
+// //     ...data
+// //   }
+
+// //   console.log('Ford data:', formData)
+// // }
+
+// // Rest
+// function saveForm(...args) {
+  
+//   const [title, text, description] = args
+
+//   const formData = {
+//     date: new Date().toLocaleDateString(),
+//     title, text, description
+//   }
+
+//   console.log('Ford data:', formData)
+
+// ES6 строки
+// const createLink = ({path, name}) => {
+// 	return `<a target="_blank" href="${path}">${name}</a>`
+// }
+
+// const ul = document.querySelector('ul')
+// const google = `<li>${createLink({path: 'https://google.com', name: 'Google'})}</li>`
+// const yandex = `<li>${createLink({path: 'https://ya.ru', name: 'Yandex'})}</li>`
+
+// ul.insertAdjacentHTML('afterbegin', google)
+// ul.insertAdjacentHTML('afterbegin', yandex)
